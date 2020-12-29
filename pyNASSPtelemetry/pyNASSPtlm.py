9f92af321bb337f9e49484bfcfaefa09cb326923
Matthew Hume <mtthwhm@gmail.com>
2020-12-29 15:06:59 -0500

    added lem and csm logging to seperate files

--- pyNASSPtelemetry/pyNASSPtlm.py
+++ pyNASSPtelemetry/pyNASSPtlm.py
@@ -7,13 +7,56 @@ if __name__ == '__main__':
     
     import socket
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('192.168.1.19', 14242))
-    a = 500
-    while True:
-        data = s.recv(1024)
-        if not data:
-            break
-        print(repr(data))
-    
-    s.close()
+    CSM = 1 
+    LEM = 1
+
+    CSMlogFileName = 'CSMtelemetryLog.txt'
+    LEMlogFileName = 'LEMtelemetryLog.txt'
+    ipAddress = '192.168.1.19'
+    CSMport = 14242
+    LEMport = 14243
+
+    CSMlogFile = open(CSMlogFileName,'a')
+    LEMlogFile = open(LEMlogFileName,'a')
+
+    CSMlogFile.write('NEWLOG\n')
+    LEMlogFile.write('NEWLOG\n')
+
+    CSMsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    LEMsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    CSMsocket.connect((ipAddress, CSMport))
+    LEMsocket.connect((ipAddress, LEMport))
+
+    try:
+
+        while True:
+            if CSM == 1:
+                CSMdata = CSMsocket.recv(1024)
+                if not CSMdata:
+                    break
+        
+                CSMbuffer = memoryview(CSMdata).cast('B')
+        
+                for ii in CSMbuffer:
+                    CSMlogFile.write(CSMbuffer[ii] + '\n')
+
+            if LEM == 1:
+                LEMdata = LEMsocket.recv(1024)
+                if not LEMdata:
+                    break
+
+                LEMbuffer = memoryview(LEMdata).cast('B')
+
+                for jj in LEMbuffer:
+                    LEMlogFile.write(CSMbuffer[jj] + '\n')
+
+    except KeyboardInterupt:
+
+        CSMlogFile.close()
+        LEMlogFile.close()
+
+        CSMsocket.close()
+        LEMsocket.close()
+
+
